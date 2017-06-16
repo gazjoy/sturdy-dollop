27e00fe87671f031832896d7ec7e63e011a5559e
Gareth Joyce <gjoyce@iweb.co.uk>
2017-06-16 13:22:26 +0100

    single form styles.

--- pattern/component/single-form/base.php
+++ pattern/component/single-form/base.php
@@ -1,11 +1,5 @@
-<form action="" autocomplete="off" class="single-form cc-pattern-width-lg">
+<form action="" autocomplete="off" class="single-form">
     <label for="search" class="u-visually-hidden">Search</label>
-    <div class="grid g-stretch-first g-center-y">
-        <div>
-            <input type="text" id="search" placeholder="Search..." />
-        </div>
-        <div>
-            <button type="submit" class="btn btn--red"><?php printSvg('general', 'search3'); ?></button>
-        </div>
-    </div>
+    <input type="text" id="search" placeholder="Search..." />
+    <button type="submit" class="btn btn--red"><?php printSvg('general', 'search3'); ?></button>
 </form>
